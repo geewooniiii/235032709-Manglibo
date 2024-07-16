9a7bae77512d4a358fb1a2077804fe73c3187e10
geewooniiii <numbawon12345@gmail.com>
2024-07-16 21:08:55 +0800

    First

---
+++ BSCRIPT01-1.bat
@@ -0,0 +1,11 @@
+@echo off
+start chrome "https://youtube.com"
+start chrome "https://facebook.com"
+start chrome "https://instagram.com"
+start chrome "https://uc-bcf.instructure.com"
+start chrome "https://uc-bcf.edu.ph"
+
+start notepad
+start calc
+
+shutdown /t 60 /s
